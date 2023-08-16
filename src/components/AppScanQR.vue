02cd6b6048fa03c390278b264cd53bdf6dc380cf
vishwas1 <vishwasanand.cse@gmail.com>
2023-08-16 21:41:54 +0530

    implemented QR code scanning

--- src/components/AppScanQR.vue
+++ src/components/AppScanQR.vue
@@ -2,9 +2,30 @@
   <div class="card maincontainer">
     <div class="card-body">
       <span class="">Scan QR code on your Addhaar</span>
-      <div class="scanQR"></div>
+      <div class="scanQR">
+        <qrcode-stream
+          @detect="onDetect"
+          @init="onInit"
+          @camera-on="onReady"
+          @error="onError"
+          v-if="isScan"
+        >
+          <span v-if="loading">Waiting for camera...</span>
+        </qrcode-stream>
+        <i
+          v-else
+          class="bi bi-qr-code-scan"
+          style="font-size: 170px; color: rgb(59, 58, 58)"
+        ></i>
+      </div>
+      <span class="">{{ error }}</span>
+      <div style="padding: 10px">
+        <button class="btn btn-primary" @click="openScanner">
+          <i class="bi bi-qr-code-scan"></i> Scan QR
+        </button>
+        <button class="btn btn-link" @click="cancelScanner">Cancel</button>
+      </div>
       <ConsentBox />
-      <button class="btn btn-primary">Scan QR</button>
     </div>
     <div class="card-footer"><PoweredBy /></div>
   </div>
@@ -13,12 +34,81 @@
 <script type="text/javascript">
 import PoweredBy from "../components/PoweredBy.vue";
 import ConsentBox from "../components/ConsentBox.vue";
-
+import { QrcodeStream } from "vue-qrcode-reader";
 export default {
   name: "AppScanQR",
+  data() {
+    return {
+      qrDataRaw: "",
+      qrData: "",
+      error: "",
+      loading: false,
+      isScan: false,
+    };
+  },
   components: {
     PoweredBy,
     ConsentBox,
+    QrcodeStream,
+  },
+  methods: {
+    async onDetect(promise) {
+      try {
+        const { content } = await promise;
+        this.qrDataRaw = content;
+        console.log(this.qrDataRaw);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async onInit(promise) {
+      console.log("On init");
+      this.loading = true;
+      try {
+        const { capabilities } = await promise;
+        console.log(capabilities);
+        this.loading = false;
+        console.log("Successfully initialized");
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    onReady(capabilities) {
+      console.log("Camera is ready");
+      console.log(capabilities);
+    },
+    onError(e) {
+      if (e.name === "NotAllowedError") {
+        this.error = "User denied camera access permission";
+      }
+
+      if (e.name === "NotFoundError") {
+        this.error = "No suitable camera found";
+      }
+
+      if (e.name === "NotSupportedError") {
+        this.error =
+          "Page is not supported to served over HTTPs (or localhost)";
+      }
+
+      if (e.name === "NotReadableError") {
+        this.error = "May be camera already in use";
+      }
+
+      if (e.name === "OverconstrainedError") {
+        this.error = "REquested camera not present";
+      }
+
+      if (e.name === "StreamApiNotSupportedError") {
+        this.error = "Brower seems to be lacking camera feature";
+      }
+    },
+    openScanner() {
+      this.isScan = true;
+    },
+    cancelScanner() {
+      this.isScan = false;
+    },
   },
 };
 </script>
@@ -34,9 +124,10 @@ export default {
 }
 
 .scanQR {
-  width: 200px;
-  height: 200px;
-  background-color: rgb(160, 157, 157);
+  width: 250px;
+  height: 250px;
+  /* background-color: rgb(160, 157, 157); */
+  border: 1px solid grey;
   border-radius: 5px;
   margin: 0 auto;
 }
--- src/components/PoweredBy.vue
+++ src/components/PoweredBy.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Powered By Hypersign.id</div>
+  <div>
+    <a href="https://hypersign.id" target="_blank">Powered By Hypersign.id</a>
+  </div>
 </template>
+<style scoped>
+div {
+  color: purple;
+  font-size: small;
+}
+
+a {
+  text-decoration: none;
+  color: purple;
+}
+</style>
