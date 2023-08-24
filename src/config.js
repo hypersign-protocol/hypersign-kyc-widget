395bd65152358e50cd35889054669d2f39b519ef
Pratap Mridha <pratapmridha@gmail.com>
2023-08-24 15:37:05 +0530

    updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const KAVACH_SERVER_BASE_URL = "http://localhost:3000"
 export const ENTITY_API_BASE_URL = "https://api.entity.hypersign.id"
-export const ENTITY_APP_SERCRET=''
+export const ENTITY_APP_SERCRET='0a88981d30e66a8b188760b081c14.02178535aa9c0ee9fe6cf4e2e3902559f12357c93d44df1959229122def5be650303272f2dc4d5443cb7275c25e2566e5'
 export const KAVACH_SERVER_BASE_URL = "https://stage.hypermine.in/kavach"
