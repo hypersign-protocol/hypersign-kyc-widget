e750bebb2664cb5b0f228d25ed2045dfc52415e6
vishwas1 <vishwasanand.cse@gmail.com>
2024-02-29 00:05:41 +0530

    updated

--- src/components/AppInstructions.vue
+++ src/components/AppInstructions.vue
@@ -3,11 +3,10 @@
     <load-ing :active.sync="isLoading" :can-cancel="true" :is-full-page="fullPage"></load-ing>
     <NavBar />
     <div class="card-body">
-      <PageHeading :header="'Verify Your Identity Using Aadhaar'"
-        :subHeader="'Follow these simple instructions to verify'" />
+      <PageHeading :header="'Cavach KYC'" :subHeader="'Follow these simple instructions for your KYC request'" />
       <div class="">
         <img class="opacity-80" src="../assets/page0.png" style="padding: 20px; height:500px; width: 70%" width="100%" />
-        <div class="d-grid gap-1">
+        <div class="d-grid gap-1 " style="width: 50%;margin: auto;">
           <button class="btn btn-outline-dark" @click="nextStep()">
             Let's go!
           </button>
