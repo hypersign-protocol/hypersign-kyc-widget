69234096c2117b046160502fd857830f2bc1c911
vishwas1 <vishwasanand.cse@gmail.com>
2024-03-26 16:03:39 +0530

    update widget config

--- src/components/utils/widget.config.js
+++ src/components/utils/widget.config.js
@@ -1,17 +1,17 @@
 export default {
     steps: {
         faceRecog: true,
-        idOcr: false,
+        idOcr: true,
     },
     schemaIds: {
         PersonhoodCredential: {
             schemaId: "sch:hid:testnet:z6Mkvtd73dDgg7HU8wLCmXbe2RAHPAU1Ex1VUXCFtPV7u36i:1.0",
             issuer: "did:hid:testnet:zCyAz2wfKjAaWE4FW75KxpZh2wuo9kRAUZyV2xEe93cKr"
         },
-        // PassportCredential: {
-        //     schemaId: "sch:hid:testnet:z6MkgMXXQL7YD7BufNLbjrwueoj4nmih9xujJ6aozJDmzFWx:1.0",
-        //     issuer: "did:hid:testnet:zCyAz2wfKjAaWE4FW75KxpZh2wuo9kRAUZyV2xEe93cKr"
-        // }
+        PassportCredential: {
+            schemaId: "sch:hid:testnet:z6MkgMXXQL7YD7BufNLbjrwueoj4nmih9xujJ6aozJDmzFWx:1.0",
+            issuer: "did:hid:testnet:zCyAz2wfKjAaWE4FW75KxpZh2wuo9kRAUZyV2xEe93cKr"
+        }
 
         // CitizenshipCredential: {
         //     schemaId: "sch:hid:testnet:z6Mkkm7paF78CWtrpviRkwb83t99u34Up7tjjnap8yw1pWfz:1.0",
