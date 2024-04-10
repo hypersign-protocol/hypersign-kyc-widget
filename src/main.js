f130f1c8719938bcaa03b5c00984dd4407b8f736
vishwas1 <vishwasanand.cse@gmail.com>
2024-04-10 18:09:46 +0530

    removed vue bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap-icons/font/bootstrap-icons';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
