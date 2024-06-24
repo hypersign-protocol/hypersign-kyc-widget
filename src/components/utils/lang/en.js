5d5feb0ca8b8883940d4722b8e8f6ec271bc61d2
vishwas1 <vishwasanand.cse@gmail.com>
2024-06-24 14:53:52 +0530

    added messages for signpage

---
+++ src/components/utils/lang/en.js
@@ -0,0 +1,10 @@
+
+
+export default {
+    SIGN: {
+        WIDGET_INIT_ERR: 'Error initalization of widget',
+        WIDGET_CONFIG_ERR: 'Widget configuration is not available',
+        INVALID_ACCESS_TOKEN: 'Error initalization of widget, invalid token',
+        AUTH_PROVIDER_ERR: 'Could not authenticate with provider '
+    }
+}
