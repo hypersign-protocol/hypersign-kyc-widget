b412d3223a48c15e8909e1b91dbf0676e584eb87
vishwas1 <vishwasanand.cse@gmail.com>
2024-11-20 11:34:59 +0530

    updated eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,19 @@ module.exports = {
     // semi: ['error', 'always'],
     'no-async-promise-executor': 'off',
     'no-console': 'warn',
+    'max-len': [
+      'error',
+      5000,
+      2,
+      {
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: false,
+        ignoreTemplateLiterals: false,
+      },
+    ],
+    'vue/max-attributes-per-line': 'off',
+    'vue/single-attribute-per-line': 'off',
+    'space-before-function-paren': 'off',
   },
 }
