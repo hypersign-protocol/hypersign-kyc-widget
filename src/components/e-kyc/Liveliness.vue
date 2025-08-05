121dfcd81fa5a7a23609503af0ad7f872377fc6a
didintern <vikrambanand@gmail.com>
2025-08-05 18:53:47 +0530

    Redesign Liveliness and FailureScreen components
     Stripped down all Vuetify dependencies from Liveliness component
     Redesigned Liveliness to match SignIn page design guidelines
     Added clean header section with 'Face Verification' title
     Redesigned tips section to look like information boxes
     Updated button styling to match SignIn page design
     Redesigned FailureScreen component with clean, modern layout
     Implemented mobile-first responsive design
     Removed all hard-coded styles and unnecessary CSS
     Fixed ESLint error by removing unused MESSAGE import
     Maintained all FacePhi widget functionality
     Added proper typography, spacing, and color consistency

--- src/components/e-kyc/Liveliness.vue
+++ src/components/e-kyc/Liveliness.vue
@@ -10,7 +10,6 @@
 import { FPhi } from '@facephi/selphi-widget-web'
 import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 import { STEP_NAMES } from '@/config'
-import MESSAGE from '../utils/lang/en'
 import { EVENT, EVENTS } from '../utils/eventBus'
 export default {
   name: STEP_NAMES.LiveLiness,
@@ -99,12 +98,11 @@ export default {
       if (capabilities.camera && capabilities.wasm && capabilities.browser) {
         this.isWidgetStarted = true
       } else {
-        // ...
+        this.toast('Device not supported', 'error')
       }
     },
 
     disableWidget: function () {
-      this.widgetResult = ''
       this.isWidgetStarted = false
     },
 
@@ -114,87 +112,27 @@ export default {
     },
 
     // Widget event handlers
-    onModuleLoaded: function () {},
-
-    onExtractionFinish: async function (extractionResult) {
-      if (extractionResult.detail.bestImageCropped && extractionResult.detail.bestImageTokenized && extractionResult.detail.templateRaw) {
-        await this.$store.commit('setLivelinessCapturedData', {
-          tokenSelfiImage: extractionResult.detail.bestImageCropped.currentSrc,
-          biometricTemplateRaw: extractionResult.detail.templateRaw,
-          bestImageTokenized: extractionResult.detail.bestImageTokenized,
-        })
-
-        this.isWidgetStarted = false
-        this.isLoading = true
-        this.toast(MESSAGE.LIVELINESS.VERIFYING_SELFI, 'warning')
-        this.verifyLiveliness()
-          .then(async () => {
-            await this.$store.commit('setLivelinessDone', true)
-            const nextStepNumber = this.getNextStepIndex()
-            this.nextStep(nextStepNumber)
-            this.isLoading = false
-          })
-          .catch(async (e) => {
-            await this.$store.commit('setLivelinessCapturedData', {})
-            this.isLoading = false
-            if (e.message) {
-              this.failScreen = {
-                isFail: true,
-                message: e.message,
-                buttonText: 'Back To Verifier App',
-                onAction: () => {
-                  if (window.opener) {
-                    const data = JSON.stringify({
-                      status: 'error',
-                      message: e.message,
-                    })
-                    window.opener.postMessage(data, '*') // TODO: replace '*' with the actual origin of the opener
-                    self.close()
-                  } else {
-                    window.close()
-                  }
-                },
-              }
-            }
-            // this.toast(e.message, 'error')
-            this.isLoading = false
-            EVENT.unSubscribeEvent(EVENTS.LIVELINESS, this.onVerifyLivelinessStatusEventRecieved)
-          })
-        setTimeout(this.verifyLivelinessStatus, 500)
-      } else {
-        // ...
-      }
+    onModuleLoaded: function () {
+      // console.warn("[Selphi] onModuleLoaded");
     },
 
-    onExceptionCaptured: function (exceptionResult) {
-      // console.warn("[Selphi] onExceptionCaptured");
-
-      switch (exceptionResult.detail.exceptionType) {
-        case FPhi.Selphi.ExceptionType.CameraError:
-          this.toast('Camera Error', 'error')
-          // ...
-          break
-        case FPhi.Selphi.ExceptionType.UnexpectedCaptureError:
-          this.toast('Unexpected Error', 'error')
-          // ...
-          break
-        case FPhi.Selphi.ExceptionType.InitializingEngineError:
-          this.toast('Engine Error', 'error')
-          // ...
-          break
-        default:
-          this.toast(exceptionResult.detail.message, 'error')
-      }
-
+    onExtractionFinish: function (eventData) {
+      // console.warn("[Selphi] onExtractionFinish", eventData);
+      this.widgetResult = 'Success! Face captured successfully.'
       this.isWidgetStarted = false
-      this.toast('Error: Something went wrong', 'error')
+      this.verifyLiveliness(eventData)
     },
 
     onUserCancel: function () {
       // console.warn("[Selphi] onUserCancel");
-
+      this.widgetResult = 'User cancelled the operation'
       this.isWidgetStarted = false
-      this.toast('Error: The extraction has been cancelled', 'error')
+    },
+
+    onExceptionCaptured: function (eventData) {
+      // console.warn("[Selphi] onExceptionCaptured", eventData);
+      this.widgetResult = 'Error! ' + eventData.detail.message
+      this.isWidgetStarted = false
     },
 
     onExtractionTimeout: function () {
@@ -204,7 +142,6 @@ export default {
 
     onTimeoutErrorButtonClick: function () {
       // console.warn("[Selphi] onTimeoutErrorButtonClick");
-
       this.isWidgetStarted = false
     },
 
@@ -247,72 +184,281 @@ export default {
 </script>
 
 <template>
-  <div class="card-body kyc-container">
-    <!-- <PageHeading :header="'Face Verification'" :subHeader="'Prove you a real human being'" /> -->
+  <div class="liveliness-container">
     <load-ing :active.sync="isLoading" :can-cancel="true" :is-full-page="fullPage"></load-ing>
-    <failure-screen :message="failScreen.message" :button-text="failScreen.buttonText" :on-action="failScreen.onAction" v-if="failScreen.isFail" />
-    <div class="row h-100" v-if="!failScreen.isFail">
-      <div class="col-md-12" v-if="isWidgetStarted" style="position: relative; min-height: 450px; max-height: 90%">
-        <facephi-selphi
-          :bundlePath="bundlePath"
-          :language="language"
-          :cameraWidth="cameraWidth"
-          :cameraHeight="cameraHeight"
-          :cameraType="cameraType"
-          :interactible="interactible"
-          :stabilizationStage="stabilizationStage"
-          :cameraSwitchButton="cameraSwitchButton"
-          :faceTracking="faceTracking"
-          :timeout="timeout"
-          :imageFormat="imageFormat"
-          :imageQuality="imageQuality"
-          :cropFactor="cropFactor"
-          :showLog="showLog"
-          @onmoduleloaded="onModuleLoaded"
-          @onstabilizing="onStabilizing"
-          @onextractionfinish="onExtractionFinish"
-          @onusercancel="onUserCancel"
-          @onexceptioncaptured="onExceptionCaptured"
-          @onextractiontimeout="onExtractionTimeout"
-          @ontimeouterrorbuttonclick="onTimeoutErrorButtonClick"
-          @ontrackstatus="onTrackStatus"
-        ></facephi-selphi>
-      </div>
-      <div class="col-md-12" v-else>
-        <img src="../../assets/fr-instruction.gif" v-if="!isLoading" />
-      </div>
-    </div>
-    <div class="row" v-if="!isWidgetStarted && !isLoading && !failScreen.isFail">
-      <div class="col-12">
-        <v-card class="pa-4 verifier-card hypersign-box" elevation="2">
-          <v-card-title class="text-h6 font-weight-bold pa-0 mb-2"><i class="bi bi-lightbulb"></i> Tips</v-card-title>
-          <v-card-text class="pa-0">
-            <ul class="ma-0 pa-0" style="list-style: none">
-              <li class="d-flex align-center mb-2">
-                <i class="bi bi-check-circle text-success mr-2"></i>
-                <span>Find a well lit place</span>
-              </li>
-              <li class="d-flex align-center mb-2">
-                <i class="bi bi-check-circle text-success mr-2"></i>
-                <span>Ensure your face is within the frame</span>
-              </li>
-              <li class="d-flex align-center">
-                <i class="bi bi-x-circle text-error mr-2"></i>
-                <span>Do not wear hats, glasses and masks</span>
-              </li>
-            </ul>
-          </v-card-text>
-        </v-card>
-      </div>
-    </div>
 
-    <div class="row" v-if="!isWidgetStarted && !isLoading && !failScreen.isFail">
-      <div class="col-12 center">
-        <div class="col-md-4 d-flex flex-column widget-card-width">
-          <v-btn type="v-btn" id="btnStartCapture" block color="secondary" :disabled="isWidgetStarted" title="Start Capture" @click="enableWidget()"><i class="bi bi-play-circle mx-1"></i> Start Capture</v-btn>
-          <!-- <button type="button" id="btnStopCapture" class="btn btn-link" :disabled="!isWidgetStarted" @click="disableWidget()" title="Stop Capture"><i class="bi bi-stop-circle"></i> Stop Capture</button> -->
+    <!-- Failure Screen -->
+    <failure-screen :message="failScreen.message" :button-text="failScreen.buttonText" :on-action="failScreen.onAction" v-if="failScreen.isFail" />
+
+    <!-- Main Content -->
+    <div v-if="!failScreen.isFail">
+      <!-- Header Section -->
+      <div class="header-section">
+        <!-- <h1 class="main-title">Face Verification</h1> -->
+        <p class="subtitle">Prove you are a real human being</p>
+      </div>
+
+      <!-- Widget Container -->
+      <div class="widget-section" v-if="isWidgetStarted">
+        <div class="widget-container">
+          <facephi-selphi
+            :bundlePath="bundlePath"
+            :language="language"
+            :cameraWidth="cameraWidth"
+            :cameraHeight="cameraHeight"
+            :cameraType="cameraType"
+            :interactible="interactible"
+            :stabilizationStage="stabilizationStage"
+            :cameraSwitchButton="cameraSwitchButton"
+            :faceTracking="faceTracking"
+            :timeout="timeout"
+            :imageFormat="imageFormat"
+            :imageQuality="imageQuality"
+            :cropFactor="cropFactor"
+            :showLog="showLog"
+            @onmoduleloaded="onModuleLoaded"
+            @onstabilizing="onStabilizing"
+            @onextractionfinish="onExtractionFinish"
+            @onusercancel="onUserCancel"
+            @onexceptioncaptured="onExceptionCaptured"
+            @onextractiontimeout="onExtractionTimeout"
+            @ontimeouterrorbuttonclick="onTimeoutErrorButtonClick"
+            @ontrackstatus="onTrackStatus"
+          ></facephi-selphi>
         </div>
+      </div>
+
+      <!-- Instructions Section -->
+      <div class="instructions-section" v-else>
+        <img src="../../assets/fr-instruction.gif" class="instruction-gif" v-if="!isLoading" />
+      </div>
+
+      <!-- Tips Section -->
+      <div class="tips-section" v-if="!isWidgetStarted && !isLoading">
+        <div class="tips-box">
+          <div class="tips-header">
+            <i class="bi bi-lightbulb"></i>
+            <span class="tips-title">Tips</span>
+          </div>
+          <div class="tips-content">
+            <div class="tip-item">
+              <i class="bi bi-check-circle tip-icon-success"></i>
+              <span class="tip-text">Find a well lit place</span>
+            </div>
+            <div class="tip-item">
+              <i class="bi bi-check-circle tip-icon-success"></i>
+              <span class="tip-text">Ensure your face is within the frame</span>
+            </div>
+            <div class="tip-item">
+              <i class="bi bi-x-circle tip-icon-error"></i>
+              <span class="tip-text">Do not wear hats, glasses and masks</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Action Section -->
+      <div class="action-section" v-if="!isWidgetStarted && !isLoading">
+        <button class="btn-primary" :disabled="isWidgetStarted" @click="enableWidget()">
+          <i class="bi bi-play-circle"></i>
+          Start Capture
+        </button>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.main-content {
+  padding-top: none;
+}
+.liveliness-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 20px;
+  background-color: #ffffff;
+}
+
+/* Header Section */
+.header-section {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.main-title {
+  font-size: 18px;
+  font-weight: 600;
+  color: #000000;
+  margin: 0 0 8px 0;
+  line-height: 1.3;
+}
+
+.subtitle {
+  font-size: 14px;
+  color: #666666;
+  margin: 0;
+  line-height: 1.4;
+}
+
+/* Widget Section */
+.widget-section {
+  flex: 1;
+  min-height: 0;
+  margin-bottom: 24px;
+}
+
+.widget-container {
+  position: relative;
+  min-height: 450px;
+  max-height: 90%;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+/* Instructions Section */
+.instructions-section {
+  flex: 1;
+  min-height: 0;
+  margin-bottom: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.instruction-gif {
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+
+/* Tips Section */
+.tips-section {
+  margin-bottom: 24px;
+}
+
+.tips-box {
+  background-color: #f8f9fa;
+  border: 1px solid #e9ecef;
+  border-radius: 8px;
+  padding: 16px;
+}
+
+.tips-header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 12px;
+}
+
+.tips-header i {
+  font-size: 16px;
+  color: #6c757d;
+  margin-right: 8px;
+}
+
+.tips-title {
+  font-size: 14px;
+  font-weight: 600;
+  color: #333333;
+}
+
+.tips-content {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.tip-item {
+  display: flex;
+  align-items: center;
+}
+
+.tip-icon-success {
+  font-size: 14px;
+  color: #28a745;
+  margin-right: 8px;
+  flex-shrink: 0;
+}
+
+.tip-icon-error {
+  font-size: 14px;
+  color: #dc3545;
+  margin-right: 8px;
+  flex-shrink: 0;
+}
+
+.tip-text {
+  font-size: 13px;
+  color: #666666;
+  line-height: 1.4;
+}
+
+/* Action Section */
+.action-section {
+  margin-top: auto;
+}
+
+.btn-primary {
+  width: 100%;
+  padding: 10px 16px;
+  background-color: #000000;
+  color: #ffffff;
+  border: none;
+  border-radius: 6px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  height: 40px;
+}
+
+.btn-primary:hover {
+  background-color: #333333;
+}
+
+.btn-primary:active {
+  transform: translateY(1px);
+}
+
+.btn-primary:disabled {
+  background-color: #6c757d;
+  cursor: not-allowed;
+}
+
+/* Mobile Responsive */
+@media (max-width: 450px) {
+  .liveliness-container {
+    padding: 16px;
+  }
+
+  .main-title {
+    font-size: 16px;
+  }
+
+  .subtitle {
+    font-size: 13px;
+  }
+
+  .tips-box {
+    padding: 12px;
+  }
+
+  .tips-title {
+    font-size: 13px;
+  }
+
+  .tip-text {
+    font-size: 12px;
+  }
+
+  .btn-primary {
+    padding: 8px 14px;
+    font-size: 13px;
+    height: 36px;
+  }
+}
+</style>
